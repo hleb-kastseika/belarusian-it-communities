db1873b7d057da364cc5a31f443985fb7eabdb7c
Gleb Kosteiko <gleb.kosteiko@gmail.com>
2020-07-10 14:14:13 +0300

    Add supporting of Linkedin captcha status codes

--- .github/workflows/check_links.sh
+++ .github/workflows/check_links.sh
@@ -3,7 +3,7 @@
 cat ../../README.md | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | while read url
 do
   status=$(curl -I -L -X GET $url 2>/dev/null | head -n 1 | cut -d$' ' -f2)
-  if [[ "$status" -eq 200 || "$status" -eq 301 || "$status" -eq 302 ]] ; then
+  if [[ "$status" -eq 200 || "$status" -eq 301 || "$status" -eq 302 || "$status" -eq 999 ]] ; then
     echo "URL exists: $url - $status"
   else
     echo "URL does not exist: $url - $status"
