71d8140b7bda3a0893b30f08f912bda1304e7f78
Gleb Kosteiko <gleb.kosteiko@gmail.com>
2020-07-10 14:02:36 +0300

    Add links checking

--- .github/workflows/check_links.sh
+++ .github/workflows/check_links.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hi"
+cat ../../README.md | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | while read url
+do
+  status=$(curl -I -L -X GET $url 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+  if [[ "$status" -eq 200 || "$status" -eq 301 || "$status" -eq 302 ]] ; then
+    echo "URL exists: $url - $status"
+  else
+    echo "URL does not exist: $url - $status"
+    exit 1
+  fi
+  sleep .5
+done
